31a73d1223fd510c7fdd4c78a4db21bf70f63919
Erik Rasmussen <erasmussen@xebialabs.com>
2017-02-16 13:27:09 -0600

    Trying to use Selenium within XLR.

--- src/main/resources/selenium/Selenium.py
+++ src/main/resources/selenium/Selenium.py
@@ -8,6 +8,10 @@
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 
+from selenium import webdriver
+#from selenium.common.exceptions import TimeoutException
+#from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
+#from selenium.webdriver.support import expected_conditions as EC # available since 2.26.0
 
 class SeleniumClient(object):
     def __init__(self):
@@ -17,6 +21,32 @@ class SeleniumClient(object):
     def get_client():
         return SeleniumClient()
 
-    def selenium_executetest(self, variables):
-        test_name = variables['test_name']
-        return {"output" : "Successfully executed test: %s" % test_name}
+    def testing_seleniumexample(self, variables):
+        print "Starting Selenium Test Example..."
+        output = ""
+        desired_cap = {
+            'browserName': "firefox",
+        }
+        driver = webdriver.Remote(
+            command_executor='http://localhost:4444/wd/hub',
+            desired_capabilities=desired_cap)
+        print "driver: %s" % driver
+        # go to the google home page
+        driver.get("http://www.google.com")
+        # the page is ajaxy so the title is originally this:
+        output += "%s\n" % driver.title
+        # find the element that's name attribute is q (the google search box)
+        inputElement = driver.find_element_by_name("q")
+        # type in the search
+        inputElement.send_keys("cheese!")
+        # submit the form (although google automatically searches now without submitting)
+        inputElement.submit()
+        try:
+            # we have to wait for the page to refresh, the last thing that seems to be updated is the title
+            WebDriverWait(driver, 10).until(EC.title_contains("cheese!"))
+
+            # You should see "cheese! - Google Search"
+            output += "%s\n" % driver.title
+        finally:
+            driver.quit()
+        return {"output" : "SUCCESS"}
